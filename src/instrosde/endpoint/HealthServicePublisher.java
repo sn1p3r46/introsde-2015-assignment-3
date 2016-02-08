b2335a8402c8799b235916b0331f854bc4ae87a2
sn1p3r46 <kingokongo46@gmail.com>
2016-02-09 00:05:09 +0100

    still tring

--- src/instrosde/endpoint/HealthServicePublisher.java
+++ src/instrosde/endpoint/HealthServicePublisher.java
@@ -31,36 +31,5 @@ public class PeoplePublisher {
         System.out.println("Starting People Service...");
         System.out.println("--> Published. Check out "+endpointUrl+"?wsdl");
         Endpoint.publish(endpointUrl, new HealthServiceImplementation());
-    }
-}
-
-import java.net.InetAddress;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.io.IOException;
-
-import javax.xml.ws.Endpoint;
-
-import introsde.assignment.soap.PeopleImpl;
-
-public class PeoplePublisher {
-    public static void main(String[] args) throws IllegalArgumentException, IOException, URISyntaxException{
-        String PROTOCOL = "http://";
-        String HOSTNAME = InetAddress.getLocalHost().getHostAddress();
-        if (HOSTNAME.equals("127.0.0.1"))
-        {
-            HOSTNAME = "localhost";
         }
-        String PORT = "6902";
-        String BASE_URL = "/ws/health";
-
-        if (String.valueOf(System.getenv("PORT")) != "null"){
-            PORT=String.valueOf(System.getenv("PORT"));
-        }
-
-        String endpointUrl = PROTOCOL+HOSTNAME+":"+PORT+BASE_URL;
-        System.out.println("Starting People Service...");
-        System.out.println("--> Published. Check out "+endpointUrl+"?wsdl");
-        Endpoint.publish(endpointUrl, new HealthServiceImplementation());
-    }
-}
+    }   
